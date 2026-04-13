fcc262ed50c4b8b5d3f56203eb09dc9d45184e92
vedant_dagade <vedantdagade21@gmail.com>
2026-04-13 11:46:15 +0530

    Product controller versioning added

--- src/main/java/com/classicmodels/classicmodels/controller/ProductController.java
+++ src/main/java/com/classicmodels/classicmodels/controller/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
 
